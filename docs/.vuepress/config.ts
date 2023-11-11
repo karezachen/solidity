465f259dd9d6679c277caeced6cf1315c4b5b3c9
kareza <kareza@qq.com>
2023-11-12 01:39:33 +0800

    新增logo

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,14 @@
 import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
+  base: '/solidity/',
+
   lang: 'zh-CN',
   title: 'Hello Solidity!',
-  description: '一起快乐学习solidity吧～👏',
-  base: '/solidity/',
+  description: '一起快乐学习solidity吧～ 👏',
+})
+
+theme: defaultTheme({
+  logo: 'images/hero.png'
 })

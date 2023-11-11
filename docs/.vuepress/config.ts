e512ed3dfaa6cfa83dc92c21c06fba131422ee48
kareza <kareza@qq.com>
2023-11-11 23:03:19 +0800

    修复静态资源加载失败问题

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: 'Hello Solidity!',
+  description: '一起快乐学习solidity吧～👏',
+  base: '/solidity/',
+})
